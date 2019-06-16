3c7cb9ad81469e922b0980e71895f57f15d0abb9
devtea <devtea@protonmail.com>
2019-06-16 20:02:28 +0700

    working article card

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,7 +16,6 @@ $(document).ready(function() {
     $(`#${target.sliderId}`).slick({
       slidesToShow: 4,
       slidesToScroll: 1,
-      centerMode: true,
       prevArrow: $(`#${target.sliderPrev}`),
       nextArrow: $(`#${target.sliderNext}`),
       autoplay: false,
@@ -26,25 +25,48 @@ $(document).ready(function() {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            centerMode: false
+            slidesToShow: 3
           }
         },
         {
           breakpoint: 998,
           settings: {
-            slidesToShow: 2,
-            centerMode: false
+            slidesToShow: 2
           }
         },
         {
           breakpoint: 550,
           settings: {
             slidesToShow: 1,
-            centerMode: false
+            centerMode: true
           }
         }
       ]
     });
+  });
+
+  $(`#articleSlider`).slick({
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 3,
+    infinite: false,
+    prevArrow: $(`#article-prev`),
+    nextArrow: $(`#article-next`),
+    responsive: [
+      {
+        breakpoint: 998,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
